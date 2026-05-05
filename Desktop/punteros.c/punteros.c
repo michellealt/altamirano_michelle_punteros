f5fce212206a35ed6ebd36e9cadabff0a32a7864
(Estudiante) Michelle Stephania Altamirano Armijos <michelle.altamirano@udla.edu.ec>
2026-05-05 12:13:45 -0500

    Commit 1: Punteros con enteros

--- Desktop/punteros.c/punteros.c
+++ Desktop/punteros.c/punteros.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
-int main()
+int main(int argc, char const *argv[])
 {
-    int num = 5, a;
-    int *x;
-
+    int num=5, a;
+    int *x; 
     x = &num;
-    a = *x;
-
-    printf("%p %d\n", x, a);
+    a = *x; 
+    printf("%p %d\n",x,a);
 
     return 0;
 }
