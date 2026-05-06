c385abbfbb6d8211d64508cb83f8b8a27fe8f6f8
(Estudiante) Michelle Stephania Altamirano Armijos <michelle.altamirano@udla.edu.ec>
2026-05-05 22:51:12 -0500

    feat: estructura base del programa

--- Desktop/punteros.c/punteros.c
+++ Desktop/punteros.c/punteros.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int num=5, a;
-    int *x; 
-    x = &num;
-    a = *x; 
-    printf("%p %d\n",x,a);
-
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
